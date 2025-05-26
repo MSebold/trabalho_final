3e736d8142c2935e706467396750a06a9f774c6c
Matheus Sebold <matheusssebold@gmail.com>
2025-05-26 17:08:11 -0300

    inicial

--- trabalho_dsoo/Entidades/Indicacao.py
+++ trabalho_dsoo/Entidades/Indicacao.py
@@ -6,3 +6,15 @@ class Indicacao:
         self.__categoria = categoria
         self.__indicado = indicado
         self.__membroAcademia = membroAcademia
+
+    @property
+    def categoria(self) -> Categoria:
+        return self.__categoria
+    
+    @property
+    def indicado(self):
+        return self.__indicado
+    
+    @property
+    def membro(self):
+        return self.__membroAcademia
